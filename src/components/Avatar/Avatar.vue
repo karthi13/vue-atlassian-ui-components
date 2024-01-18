9d896294535fe6d44e608aa161ee57494601bd7b
Karthikeyan <dkarthikeyanmailbox@gmail.com>
2024-01-18 22:56:15 +0100

    feat: Avatar Component

--- src/components/Avatar/Avatar.vue
+++ src/components/Avatar/Avatar.vue
@@ -1,34 +1,69 @@
 <template>
-  <div class="flex -space-x-1 overflow-hidden">
-    <img 
-      class="w-6 h-6 bg-center bg-contain rounded-full" 
-      src="https://sierra-local.atlassian.net/wiki/aa-avatar/5da43b35bb87a40c423102a0" 
-      alt=""
-    />
-    <div class="w-6 h-6 ml-4">
-      <span class="inline-flex w-6 h-6 rounded-full bg-zinc-100">
-        <anonymous class="text-black"/>
-      </span>
-    </div>
-    
+  <div
+    role="img"
+    aria-labelledby="avatar-label"
+    class="inline-block"
+  >
+    <span class="">
+      <img 
+        class="w-10 h-10 bg-center bg-contain rounded" 
+        :src="src"
+        alt=""
+      />    
+    </span>
+    <span id="avatar-label" hidden>{{name}}</span>
   </div>
 </template>
 
 <script setup lang="ts">
-import Anonymous from './Anonymous.vue'
-
-const props = withDefaults(
-  defineProps<{
-    iconAfter?: boolean;
-  }>(),
-  {
-    iconAfter: false,
+const props = defineProps({
+  appearance: {
+    type: String,
+    default: 'circle',
   },
-);
-
-console.log(props.iconAfter);
+  borderColor: {
+    type: String,
+    default: '',
+  },
+  href: {
+    type: String,
+    default: '',
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  presence: {
+    type: String,
+    default: 'none',
+  },
+  src: {
+    type: String,
+    default: '',
+  },
+  size: {
+    type: String,
+    default: 'medium',
+  },
+  name: {
+    type: String,
+    default: 'Avatar Image',
+  },
+  target: {
+    type: String,
+    default: '',
+  },
+  status: {
+    type: String,
+    default: '',
+  },
+  tabIndex: {
+    type: Number,
+    default: 0,
+  },
+  stackIndex: {
+    type: Number,
+    default: 0,
+  },
+});
 </script>
-
-<style scoped lang="scss">
-
-</style>
