bce83466f2d90ece7752aa651832657ef68f7fa0
Karthikeyan <dkarthikeyanmailbox@gmail.com>
2024-01-18 20:10:54 +0100

    chore: remove checkbox

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <div>
     <Button />
-    <Badge />
-    <Kbd />
   </div>
 </template>
 
 <script setup lang="ts">
 import Button from './showcase/Button.vue'
-import Badge from './showcase/Badge.vue'
-import Kbd from './showcase/Kbd.vue'
 </script>
