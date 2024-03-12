89c45cf7aea1f0e4c7d6b95f0959dade8a075bdb
Karthikeyan <dkarthikeyanmailbox@gmail.com>
2024-03-12 23:15:39 +0100

    Added stories and changed itemsAfterCollapse logic and changed v-for index code

--- src/components/Breadcrumbs/Breadcrumbs.stories.ts
+++ src/components/Breadcrumbs/Breadcrumbs.stories.ts
@@ -8,19 +8,27 @@ const meta = {
   args: {
     items: [
       {
-        text: 'Home',
+        text: 'Item 1',
         href: '/',
       },
       {
-        text: 'About',
+        text: 'Item 2',
         href: '/',
       },
       {
-        text: 'Travel',
+        text: 'Item 3',
         href: '/',
       },
       {
-        text: 'Nature',
+        text: 'Item 4',
+        href: '/',
+      },
+      {
+        text: 'Item 5',
+        href: '/',
+      },
+      {
+        text: 'Item 6',
         href: '/',
       },
     ],
@@ -33,23 +41,139 @@ export const Default: Story = {
   args: {
     items: [
       {
-        text: 'Home',
+        text: 'Item 1',
         href: '/',
       },
       {
-        text: 'About',
+        text: 'Item 2',
         href: '/',
       },
       {
-        text: 'Travel',
+        text: 'Item 3',
         href: '/',
       },
       {
-        text: 'Skane',
+        text: 'Item 4',
         href: '/',
       },
     ],
   },
 };
 
+export const MoreThan8Items: Story = {
+  args: {
+    items: [
+      {
+        text: 'Item 1',
+        href: '/',
+      },
+      {
+        text: 'Item 2',
+        href: '/',
+      },
+      {
+        text: 'Item 3',
+        href: '/',
+      },
+      {
+        text: 'Item 4',
+        href: '/',
+      },
+      {
+        text: 'Item 5',
+        href: '/',
+      },
+      {
+        text: 'Item 6',
+        href: '/',
+      },
+      {
+        text: 'Item 7',
+        href: '/',
+      },
+      {
+        text: 'Item 8',
+        href: '/',
+      },
+      {
+        text: 'Item 9',
+        href: '/',
+      },
+    ],
+  },
+};
+
+export const Max4Items: Story = {
+  args: {
+    maxItems: 4,
+    items: [
+      {
+        text: 'Item 1',
+        href: '/',
+      },
+      {
+        text: 'Item 2',
+        href: '/',
+      },
+      {
+        text: 'Item 3',
+        href: '/',
+      },
+      {
+        text: 'Item 4',
+        href: '/',
+      },
+      {
+        text: 'Item 5',
+        href: '/',
+      },
+    ],
+  },
+};
+
+export const ItemsBeforeAndAfterCollapse: Story = {
+  args: {
+    maxItems: 4,
+    itemsBeforeCollapse: 3,
+    itemsAfterCollapse: 2,
+    items: [
+      {
+        text: 'Item 1',
+        href: '/',
+      },
+      {
+        text: 'Item 2',
+        href: '/',
+      },
+      {
+        text: 'Item 3',
+        href: '/',
+      },
+      {
+        text: 'Item 4',
+        href: '/',
+      },
+      {
+        text: 'Item 5',
+        href: '/',
+      },
+      {
+        text: 'Item 6',
+        href: '/',
+      },
+      {
+        text: 'Item 7',
+        href: '/',
+      },
+      {
+        text: 'Item 8',
+        href: '/',
+      },
+      {
+        text: 'Item 9',
+        href: '/',
+      },
+    ],
+  },
+};
 export default meta;
